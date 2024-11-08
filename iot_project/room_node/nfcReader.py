fb99468b3ff9a9511dc854495073918972d0b9ae
Elliotichi <e.priestley@rgu.ac.uk>
2024-11-08 10:19:28 +0000

    nfc reader code test 2

--- iot_project/room_node/nfcReader.py
+++ iot_project/room_node/nfcReader.py
@@ -1,9 +1,12 @@
+import RPI.GPIO as GPIO
 from mfrc522 import SimpleMFRC522 as nfc
 
 reader = nfc()
 
-
-text = "hello_world"
-id, text_written = reader.write(text)
-print(f"ID: {id}")
-print(f"Text Written: {text_written}")
+try:
+    text = input('New data')
+    print("place tage to write")
+    reader.write(text)
+    print("Written")
+finally:
+     GPIO.cleanup()
