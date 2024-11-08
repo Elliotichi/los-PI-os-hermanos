1728c7ef21d592c3a3fac17c933f57554b8dfdc1
sraaaaaaay <srbroch@proton.me>
2024-11-08 14:55:49 +0000

    continue webserver work

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-
 // Server config
 const server = require("express")();
-const http = require("http");
+const http = require("http").Server(server);
 const MongoClient = require('mongodb-legacy').MongoClient;
 const bodyParser = require('body-parser');
 const express = require("express");
+const { connect } = require("http2");
 io = require("socket.io")(http);
 
 session = require("express-session")({
@@ -14,28 +14,28 @@ session = require("express-session")({
 });
 
 
-const url = "something"
+const url = "mongodb+srv://visionstitch_dev:<db_password>@lospi.usv87.mongodb.net/?retryWrites=true&w=majority&appName=lospi";
 const client = new MongoClient(url);
 const dbname = 'lospi';
 
 
 server.use(session);
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+server.set("view engine", "ejs");
+server.use(express.static("public"));
+server.use(express.urlencoded({ extended: true }));
 
 var connectedSockets = {};
 var db;
 
 // Routes
 server.get("/", (req, res) => {
-    res.render("pages/chart");
+    // Landing page - should just be EJS template with a simple navbar & a search for room
 })
 
 // Websockets
 io.on("connection", (socket) => {
     connectedSockets[socket.id] = socket;
-   
+
     socket.on("room search", (room) => {
         // Produce a heatmap of the room that has been searched by the client
         // MongoDB query goes here? (overhead of rapid queries?)
@@ -48,10 +48,19 @@ io.on("connection", (socket) => {
 
 
 const connectDB = async () => {
+    // Use connect method to connect to the server
+    try {
+        await client.connect();
+        console.log('Connected successfully to MongoDB Atlas');
 
+        db = client.db(dbname);
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
-
+connectDB();
 
 http.listen(8080, () => {
     console.log("Listening on 8080");
